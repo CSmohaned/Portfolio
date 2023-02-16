981ffcf70901fdda54e5af123f10e0760e68544b
CSmohaned <mohanedadel65@gmail.com>
2023-02-16 18:59:57 +0200

    add mobile versiion

--- components/navbar.js
+++ components/navbar.js
@@ -19,41 +19,36 @@ function Navbar() {
     
     useEffect(() =>{
         setMounted(true);
-      },[])
+      },[Icon,mounted,currentTheme])
       const Menu = (e)=> {
         e.preventDefault();
         let nav = document.querySelector('nav');
         let divLeft = document.querySelector('#demo-left');
-        let moonicon = document.querySelector('#moon');
-        let Sunicon = document.querySelector('#sun');
         if(Icon === 'menu' && currentTheme ==='light') {
             setIcon('close');
             divLeft.classList.replace('md:flex', 'md:hidden');
             nav.classList.replace('md:h-14','md:h-44');
-            moonicon.classList.add('md:hidden');
         }else if (Icon === 'close' && currentTheme === 'light') {
             setIcon('menu');
             nav.classList.replace('md:h-44', 'md:h-14');
             divLeft.classList.replace('md:hidden','md:flex');
-            moonicon.classList.replace('md:hidden','md:inline');
         }
         if(Icon === 'menu' && currentTheme ==='dark') {
             setIcon('close');
             divLeft.classList.replace('md:flex', 'md:hidden');
             nav.classList.replace('md:h-14','md:h-44');
-            Sunicon.classList.add('md:hidden');
         }else if (Icon === 'close' && currentTheme === 'dark') {
             setIcon('menu');
             nav.classList.replace('md:h-44', 'md:h-14');
             divLeft.classList.replace('md:hidden','md:flex');
-            Sunicon.classList.replace('md:hidden','md:inline');
         }
     }
       const renderThemeChanger = () => {
         if(!mounted) return null;
        };
     return(
-        <nav className="md:fixed md:bottom-0 md:top-auto md:z-10 md:h-14 md:w-screen  sticky top-0 flex dark:bg-[#121212] bg-white flex-row h-14 justify-start items-center border-b-black border-b-2 w-screen z-30">
+        <>
+        <nav className="md:fixed  md:bottom-0 md:top-auto md:z-10 md:h-14 md:w-screen  sticky top-0 flex dark:bg-[#121212] bg-white flex-row h-14 justify-start items-center border-b-black border-b-2 w-screen z-30">
             {Icon=== 'close' && 
             <div className="hidden md:flex md:flex-col w-screen">
             <div className="flex flex-row justify-evenly mb-2">
@@ -92,14 +87,15 @@ function Navbar() {
                 <motion.button transition={spring}  whileHover={{scale: 1.1}}>
                 <Link className="text-[#1A1A1A] dark:text-[#FFFF] md:hidden hover:text-purple hover:dark:text-purple mr-6 flex flex-row"  href="#contact" scroll={false}><PaperAirplaneIcon className="h-5 w-5 mr-2 text-[#1A1A1A] dark:text-[#FFFF]"/>Contact</Link>
                 </motion.button>
-                <Link href="" className="md:inline flex justify-center items-center ml-5">
-                  {currentTheme !=="dark" && <MoonIcon id="moon" className="md:fixed md:right-10 md:bottom-2 absolute right-4 w-6 h-6 hover:text-gray-900 " role="button" onClick={(e) => {e.preventDefault(); setTheme('dark')}}/>}   
-                  {currentTheme ==="dark" && <SunIcon id='sun' className="md:fixed md:right-10 md:bottom-2 absolute  right-4 w-6 h-6 hover:text-yellow-500 " role="button" onClick={(e) => {e.preventDefault();setTheme('light')}}/>} 
-                  {Icon === 'menu'&& <Squares2X2Icon  name="menu-outline" onClick={(e)=>Menu(e)} className="md:bottom-2.5 md:inline hidden h-5 w-5 md:fixed md:right-3"/>}
-                  {Icon === 'close'&& <XMarkIcon id="xicon" name="close-outline" onClick={(e)=>Menu(e)} className="md:inline hidden h-5 w-5 md:fixed md:right-3 md:bottom-1 text-purple"/>} 
+                <Link href="" className="md:inline flex justify-center items-center">
+                  {Icon === 'menu'&& <Squares2X2Icon  name="menu-outline" onClick={(e)=>Menu(e)} className="md:bottom-3 md:inline hidden h-5 w-5 md:absolute md:right-4"/>}
+                  {Icon === 'close'&& <XMarkIcon id="xicon" name="close-outline" onClick={(e)=>Menu(e)} className="md:inline hidden h-5 w-5 md:absolute md:right-4 md:bottom-3 text-purple"/>} 
                 </Link>
             </div>
         </nav>
+        {currentTheme ==="dark" ? (<SunIcon id='sun' className="z-20 bg-[#FFFFFF33] px-3 py-3 text-center  rounded-tl-full rounded-bl-full  fixed right-1 top-1/2 h-12 w-12 text-white" role="button" onClick={(e) => {e.preventDefault();setTheme('light')}}/>) : 
+    (<MoonIcon id="moon" className="z-20 bg-[#0000001A] px-3 py-3 text-center  rounded-tl-full rounded-bl-full fixed right-1 top-1/2 h-12 w-12" role="button" onClick={(e) => {e.preventDefault(); setTheme('dark')}}/>)}
+        </>
         )
 }
 export default Navbar;
