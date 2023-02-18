6ac88f81bf9b002ee0631761ef5a4b87ca42f771
CSmohaned <mohanedadel65@gmail.com>
2023-02-18 09:05:22 +0200

    add new changes

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -23,7 +23,7 @@ function Portfolio() {
   <section id="portfolio"className="flex-col flex justify-center items-center  w-screen max-h-max py-10 bg-[#f5f8fc] dark:bg-silver pt-5 overflow-hidden">
     <h2 className="text-center font-bold text-4xl text-[#666666] dark:text-[#FFFFFF]">My Projects</h2>
         <span className="text-center font-normal text-sm pt-2">portfolio</span>
-        <motion.div variants={show} initial="hidden" animate="visible"  transition={spring} whileHover={{scaleX: 0.8}}  id="carouselExampleCaptions" className="carousel slide relative border rounded-2xl bg-[#1A1A1A] dark:bg-dark pt-5 mt-10 pb-8"  data-bs-ride="carousel">
+        <motion.div variants={show} initial="hidden" animate="visible"  transition={spring} whileHover={{scaleX: 0.8}}  id="carouselExampleCaptions" className="carousel slide relative border rounded-2xl bg-[#1A1A1A] dark:bg-dark pt-5 mt-10 pb-8 sm:mx-2"  data-bs-ride="carousel">
      <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
     <button
       type="button"
