6edae67011a389e5b3c126e83928a5dcf9944541
CSmohaned <mohanedadel65@gmail.com>
2023-02-18 08:35:00 +0200

    add new changes

--- pages/services.js
+++ pages/services.js
@@ -176,7 +176,7 @@ function Services() {
 <div className="z-10 modal fade fixed sm:top-0 top-5 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="exampleModalCenter4" tabIndex="-1" aria-labelledby="exampleModalCenterTitle" aria-modal="true" role="dialog">
   <div className="modal-dialog modal-dialog-centered relative w-auto pointer-events-none">
     <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white dark:bg-dark bg-clip-padding rounded-md outline-none text-current">
-      <div className="modal-header flex flex-col flex-shrink-0 items-center justify-between p-4 pt-6 md:pt-2 rounded-t-md">
+      <div className="modal-header flex flex-col flex-shrink-0 items-center justify-between p-4 pt-6  rounded-t-md">
       <button type="button"
           className="btn-close border-4 border-purple dark:border-white rounded-full box-content w-4 h-4 p-1 dark:text-white text-black opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
           data-bs-dismiss="modal" aria-label="Close"></button>
